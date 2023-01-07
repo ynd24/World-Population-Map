7ba966cec92cdf1f8d1520d355e903a18707cc59
ynd24 <yuvraj.nihal.duggal@emory.edu>
2023-01-07 01:19:26 -0500

    initial commit

---
+++ World_pop.py
@@ -0,0 +1,26 @@
+import folium
+import json
+ 
+
+ 
+data1 = json.load(open('Data/world.json', encoding='utf-8-sig'))
+ 
+
+ 
+map = folium.Map(location=[41.089742,-118.420348], tile="Stamen", zoom_start=2)
+ 
+fgp = folium.FeatureGroup(name = "World Population")    
+fgp.add_child(folium.features.GeoJson(data=data1,  
+    style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 20000000 else 'yellow' 
+        if 20000000 <= x['properties']['POP2005'] < 50000000 else 'orange' if 50000000 <= x['properties']['POP2005'] < 100000000
+            else 'red'}, tooltip = folium.features.GeoJsonTooltip(fields=['NAME','AREA','POP2005', 'LON', 'LAT'],
+                aliases=['NAME:','AREA:','POPULATION:', 'LONGITUDE:', 'LATITUDE:'],
+                    style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;"))))
+ 
+ 
+ 
+
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
+map.save("Country-Population-Map.html")
+ 
